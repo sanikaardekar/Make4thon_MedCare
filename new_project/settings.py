d35c14586c0f652075863991e3a7ff6e5c8f9c77
Arpit472002 <sarpit623@gmail.com>
2022-02-19 12:23:16 +0530

    Changed NAME in DATABASE to str()

--- new_project/settings.py
+++ new_project/settings.py
@@ -86,7 +86,7 @@ WSGI_APPLICATION = 'new_project.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': str(BASE_DIR / 'db.sqlite3'),
     }
 }
 
