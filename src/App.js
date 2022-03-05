63dbd2a14e1e1254e2b8ea380016d4f12b50d3a8
Rachana1104 <y.rachana1104@gmail.com>
2022-03-05 15:30:26 +0530

    app.js routing made

--- src/App.js
+++ src/App.js
@@ -9,6 +9,9 @@ import Aos from "aos";
 import "aos/dist/aos.css";
 import Login from './auth/Login';
 import Signup from "./auth/Signup"
+import NavbarLogin from './components/HeaderFooter/NavbarLogin';
+import Navbar from './components/HeaderFooter/Navbar';
+import NotFound from './components/NotFound';
 
 function App() {
 
@@ -20,12 +23,12 @@ function App() {
     <div className="App">
       <Router>
       <Routes>
-        <Route exact path="/" element={<><Signup/></>} />
-        <Route exact path="/login" element={<><Login /></>} />
+        <Route exact path="/" element={<><NavbarLogin/><Signup/></>} />
+        <Route exact path="/login" element={<><NavbarLogin/><Login /></>} />
         {/* <Route path="/home" element={<><HomePage/></>}></Route>
         <Route path="/yoga" element={<><Navbar /><Yoga/></>}></Route>
-        <Route path="/meditation" element={<><Navbar /><Meditation/></>}></Route>
-        <Route path="*" element={<><NotFound/></>}></Route> */}
+        <Route path="/meditation" element={<><Navbar /><Meditation/></>}></Route> */}
+        <Route path="*" element={<><NotFound/></>}></Route>
       </Routes>
     </Router>
     </div>
